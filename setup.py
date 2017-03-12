c8c1aa25ef6418888729d68fbafa946d2cddff8d
Austin Davis-Richardson <harekrishna@gmail.com>
2017-03-12 12:28:36 -0700

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='downloads',
+      version='0.0.1',
+      description='Easy HTTP downloads',
+      url='https://github.com/audy/downloads',
+      author='Austin Davis-Richardson',
+      author_email='harekrishna@gmail.com',
+      packages=['downloads'])
