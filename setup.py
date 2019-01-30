fca864f5168ae8293ab6de2a509d126b50334f1d
Austin Davis-Richardson <harekrishna@gmail.com>
2019-01-30 15:56:01 -0800

    adds long description

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
 import sys
 
-setup(name='downloads',
-      version='0.1.3',
-      description='Easy HTTP downloads',
-      url='https://github.com/audy/downloads',
-      author='Austin Davis-Richardson',
-      author_email='harekrishna@gmail.com',
-      packages=['downloads'])
+setup(
+    name="downloads",
+    version="0.1.4",
+    description="Easy HTTP downloads",
+    long_description="""
+
+      Provides an easy way to download remote files in Python. Compatible with Python 2.7+ and 3+
+      """,
+    url="https://github.com/audy/downloads",
+    author="Austin Davis-Richardson",
+    author_email="harekrishna@gmail.com",
+    packages=["downloads"],
+)
