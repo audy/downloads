91b5401abc81617d188f7512313ad6cddf1d8c8f
Austin Davis-Richardson <harekrishna@gmail.com>
2020-09-22 21:49:19 -0400

    Add type annotations

--- downloads/download.py
+++ downloads/download.py
@@ -1,34 +1,32 @@
 import os
 import sys
+from urllib.parse import urlparse
+from urllib.request import urlretrieve
 
-
-if sys.version_info > (3, 0):
-    # python 3
-    from urllib.parse import urlparse
-    from urllib.request import urlretrieve
-else:
-    # python 2
-    from urlparse import urlparse
-    from urllib import urlretrieve
+from typing import Optional
 
 
 def progress_hook(current, block_size, total_size):
-    ''' a simple progress bar '''
+    """ a simple progress bar """
 
-    proportion_downloaded = round((float(current*block_size)-block_size)/total_size, 4)
+    proportion_downloaded = round(
+        (float(current * block_size) - block_size) / total_size, 4
+    )
 
     pbar_width = int(70 * proportion_downloaded)
-    pbar = '#' * pbar_width
+    pbar = "#" * pbar_width
 
     ws_width = int((70 - (70 * proportion_downloaded)))
-    ws = ' ' * ws_width
+    ws = " " * ws_width
 
     pbar_line = "{}{}{:7.1f}%\r".format(pbar, ws, 100 * proportion_downloaded)
     sys.stderr.write(pbar_line)
 
 
-def download(url, out_path=None, progress=False):
-    '''
+def download(
+    url: str, out_path: Optional[str] = None, progress: bool = False
+) -> str:
+    """
 
     Download a file given a URL. Returns the downloaded file's local path:
 
@@ -48,12 +46,15 @@ def download(url, out_path=None, progress=False):
     There's even a fancy progress bar:
     >>> download('http://i.imgur.com/ij2h06p.png', out_path='computer.png', progress=True)
     'computer.png'
-    '''
+    """
 
     parsed = urlparse(url)
 
-    if out_path is None:
-        out_path = os.path.join(os.getcwd(), os.path.basename(parsed.path))
+    out_path = (
+        os.path.join(os.getcwd(), os.path.basename(parsed.path))
+        if out_path is None
+        else out_path
+    )
 
     if progress:
         urlretrieve(url, out_path, reporthook=progress_hook)
